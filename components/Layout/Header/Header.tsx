a2f5ddd6991ec647ef453ffe016a890ed281a11b
Pierre Velluet <pierre.velluet@willow.fr>
2021-07-19 09:36:11 +0400

    less modules and sidebar started

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,103 +1,55 @@
 import React, { useState } from "react";
-import cx from "classnames";
-import { Menu, Row, Col, Button, Popover, Badge } from "antd";
-import { SettingFilled } from "@ant-design/icons";
-import styles from "../../../styles/Header.less";
+import { Menu, Button } from "antd";
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from "@ant-design/icons";
+import classes from "./Header.module.less";
 
-const searchEngine = "Google";
+const { SubMenu } = Menu;
 
-const Header = (props: any) => {
-
-  const { isFirstScreen = false, isMoblie } = props;
-  const [menuVisible, setMenuVisible] = useState(false);
-  const menuMode = isMoblie ? "inline" : "horizontal";
-  const headerClassName = cx({
-    clearfix: true,
-    "home-nav-white": !isFirstScreen,
-  });
-
-  const onMenuVisibleChange = (visible: any) => {
-    setMenuVisible(visible);
-  };
-  const handleShowMenu = () => {
-    setMenuVisible(true);
-  };
-
-  const handleHideMenu = () => {
-    setMenuVisible(false);
-  };
-
-  const handleSelectFilter = (value: any, option: any) => {
-    const optionValue = option.props["data-label"];
-    return (
-      optionValue === searchEngine ||
-      optionValue.indexOf(value.toLowerCase()) > -1
-    );
-  };
-  const menu = [
-    <Button className="header-lang-button" ghost size="small" key="lang">
-      English
-    </Button>,
-    <Menu mode={menuMode} defaultSelectedKeys={["home"]} id="nav" key="nav">
-      <Menu.Item key="home">首页</Menu.Item>
-      <Menu.Item key="docs/spec">指引</Menu.Item>
-      <Menu.Item key="docs/react">组件</Menu.Item>
-      <Menu.Item key="docs/pattern">模式</Menu.Item>
-      <Menu.Item key="docs/resource">资源</Menu.Item>
-      <Menu.Item key="pro">
-        <a
-          href="http://pro.ant.design"
-          className="header-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          PRO
-          <span
-            style={{
-              display: "inline-block",
-              position: "relative",
-              top: -2,
-              width: 18,
-            }}
-          >
-            <Badge dot />
-          </span>
-        </a>
-      </Menu.Item>
-    </Menu>,
-  ];
+const SideBar = () => {
+  const [toggleCollapsed, setToggleColapsed] = useState(false);
 
   return (
-    <header id="header" className={cx(headerClassName, styles.header)}>
-      {menuMode === "inline" ? (
-        <Popover
-          overlayClassName="popover-menu"
-          placement="bottomRight"
-          content={menu}
-          trigger="click"
-          visible={menuVisible}
-          arrowPointAtCenter
-          onVisibleChange={onMenuVisibleChange}
-        >
-          <SettingFilled />
-        </Popover>
-      ) : null}
-      <Row>
-        <Col lg={4} md={5} sm={24} xs={24}>
-          <a id="logo">
-            <img
-              alt="logo"
-              src="https://vertikal-elagage.re"
-            />
-            <span>Ant Design</span>
-          </a>
-        </Col>
-        <Col lg={20} md={19} sm={0} xs={0}>
-          {menuMode === "horizontal" ? menu : null}
-        </Col>
-      </Row>
-    </header>
+    <Menu
+      className={classes.sidebar}
+      defaultSelectedKeys={["1"]}
+      defaultOpenKeys={["sub1"]}
+      mode="inline"
+      theme="dark"
+      inlineCollapsed={toggleCollapsed}
+    >
+      <Menu.Item key="1" icon={<PieChartOutlined />}>
+        Option 1
+      </Menu.Item>
+      <Menu.Item key="2" icon={<DesktopOutlined />}>
+        Option 2
+      </Menu.Item>
+      <Menu.Item key="3" icon={<ContainerOutlined />}>
+        Option 3
+      </Menu.Item>
+      <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+        <Menu.Item key="5">Option 5</Menu.Item>
+        <Menu.Item key="6">Option 6</Menu.Item>
+        <Menu.Item key="7">Option 7</Menu.Item>
+        <Menu.Item key="8">Option 8</Menu.Item>
+      </SubMenu>
+      <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+        <Menu.Item key="9">Option 9</Menu.Item>
+        <Menu.Item key="10">Option 10</Menu.Item>
+        <SubMenu key="sub3" title="Submenu">
+          <Menu.Item key="11">Option 11</Menu.Item>
+          <Menu.Item key="12">Option 12</Menu.Item>
+        </SubMenu>
+      </SubMenu>
+    </Menu>
   );
 };
 
-export default Header;
+export default SideBar;
